49addbefb7458c6f24eb204949fb108303088b4a
Vysakh K <vysakhk6529@gmail.com>
2020-03-24 12:04:48 +0530

    this is Vysakhs first commit -01

---
+++ app/scripts/main.js
@@ -0,0 +1,15 @@
+
+
+import Gallery from '../../../plugin/dist/gallary.umd';
+const el = document.getElementById('slider');
+ new Gallery(el);
+
+
+
+// Uncomment to enable Bootstrap tooltips
+// https://getbootstrap.com/docs/4.0/components/tooltips/#example-enable-tooltips-everywhere
+// $(function () { $('[data-toggle="tooltip"]').tooltip(); });
+
+// Uncomment to enable Bootstrap popovers
+// https://getbootstrap.com/docs/4.0/components/popovers/#example-enable-popovers-everywhere
+// $(function () { $('[data-toggle="popover"]').popover(); });
